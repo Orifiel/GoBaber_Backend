93927bec994219625251fb5cf69a2d55cc52ade7
CleitonMattos <cleitonmattos87@gmail.com>
2020-11-23 21:45:33 -0300

    first commit

---
+++ src/routes/index.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+
+import appointmentsRouter from './appointaments.routes';
+import sessionsRouter from './sessions.routes';
+import userRouter from './users.routes';
+
+const routes = Router();
+
+routes.use('/appointments', appointmentsRouter);
+
+routes.use('/users', userRouter);
+
+routes.use('/sessions', sessionsRouter);
+
+export default routes;
